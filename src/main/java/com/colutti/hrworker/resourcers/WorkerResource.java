ab78f4cc2d4728886e1005b18c6039719ed3f74b
Vinicius Colutti <viniciuscolutti@hotmail.com>
2020-12-14 08:56:32 -0300

    adicionando eureka client

--- src/main/java/com/colutti/hrworker/resourcers/WorkerResource.java
+++ src/main/java/com/colutti/hrworker/resourcers/WorkerResource.java
@@ -2,7 +2,10 @@ package com.colutti.hrworker.resourcers;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +19,11 @@ import com.colutti.hrworker.repositories.WorkerRepository;
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 
+	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+
+	@Autowired
+	private Environment env;
+
 	@Autowired
 	private WorkerRepository repository;
 
@@ -27,7 +35,7 @@ public class WorkerResource {
 
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
-
+		logger.info("PORT: " + env.getProperty("local.server.port"));
 		Worker obj = repository.findById(id).get();
 
 		return ResponseEntity.ok(obj);
