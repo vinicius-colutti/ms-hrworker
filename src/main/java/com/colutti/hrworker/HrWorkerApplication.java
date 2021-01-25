93561aade6c602df0e5930582eb9f4b6622b8250
Vinicius Colutti <viniciuscolutti@hotmail.com>
2021-01-24 21:44:03 -0300

    run in dev profile

--- src/main/java/com/colutti/hrworker/HrWorkerApplication.java
+++ src/main/java/com/colutti/hrworker/HrWorkerApplication.java
@@ -1,5 +1,6 @@
 package com.colutti.hrworker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
